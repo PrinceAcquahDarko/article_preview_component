6661c79e26bf636d09007b36f333dbda2c866ea3
Prince Darko <padarko99@gamil.com>
2020-07-05 04:38:37 -0700

    updated commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var right = document.qeurySelector('.right');
+
+var icons = document.qeurySelector('.icons')
+
+
+
+right.addEventListener('click', function(){
+	icons.classList.toggle('icons')
+})
+
+
+document.qweru
