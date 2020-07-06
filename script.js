b7c032743482f7df3fd96867d39b5bf1028a7b82
Prince Darko <padarko99@gamil.com>
2020-07-06 09:26:03 -0700

    half-way throug

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var right = document.qeurySelector('.right');
+var right = document.querySelector('.right p');
 
-var icons = document.qeurySelector('.icons')
+var icons = document.querySelector('.ico');
 
 
 
 right.addEventListener('click', function(){
+	console.log('prince')
 	icons.classList.toggle('icons')
 })
 
 
-document.qweru
